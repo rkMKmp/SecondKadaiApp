20034002d293e35084653d0dff626107dd0095d8
riki <riki13or@gmail.com>
2021-02-24 07:23:16 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by riki on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
